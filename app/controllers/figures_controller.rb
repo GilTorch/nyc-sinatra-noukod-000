b7290c5869203f66b8409cb445c78729ec362492
Gilbert Torchon <gilbert@noukod.com>
2019-01-16 23:10:44 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,7 @@ class FiguresController < ApplicationController
   # add controller methods
   get "/figures/new" do
     @titles=Title.all
-    @landmarks=[]
+    @landmarks=Landmark.all
     erb :"figures/new"
   end
 end
