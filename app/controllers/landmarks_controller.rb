44d79418bc464f1e667266aa8c23a952bab51b81
Gilbert Torchon <gilbert@noukod.com>
2019-01-16 23:43:47 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -33,8 +33,8 @@ class LandmarksController < ApplicationController
 
   patch "/landmarks/:id" do
     @landmark=Landmark.find_by_id(@params[:id])
-    @landmark.name=@params[:name]
-    @landmark.year_completed=@params[:year_completed]
+    @landmark.name=@params[:landmark][:name]
+    @landmark.year_completed=@params[:landmark][:year_completed]
     @landmark.save
     redirect "/landmarks/#{@landmark.id}"
   end
