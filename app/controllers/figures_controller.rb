a7440a716bdaf4e4d5b4c75a716b676fff902f23
Gilbert Torchon <gilbert@noukod.com>
2019-01-16 23:25:49 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,13 +8,14 @@ class FiguresController < ApplicationController
 
   get "/figures/:id/edit" do
     @figure=Figure.find_by_id(@params[:id])
+    @titles=Title.all
+    @landmarks=Landmark.all
     erb :"figures/edit"
   end
 
   get "/figures/:id" do
     @figure=Figure.find_by_id(@params[:id])
-    @titles=Title.all
-    @landmarks=Landmark.all
+
     erb :"figures/show"
   end
 
